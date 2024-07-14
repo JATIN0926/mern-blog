93fe1ed5d4d3a8a6ae276b9d84a899989ba50dfd
Jatin Chaudhary <09jatin2005@gmail.com>
2024-07-14 18:07:22 +0530

    sign in + signup done + basic UI

---
+++ api/index.js
@@ -0,0 +1,38 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
+dotenv.config();
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Connect to DB!!");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+const app = express();
+
+app.use(express.json());
+//routes
+
+app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
+
+//middlewares
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+
+  res.status(statusCode).json({
+    sucess: false,
+    statusCode,
+    message,
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Server running !!");
+});
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-red-900">Home</h1>
+    </div>
+  );
+}
