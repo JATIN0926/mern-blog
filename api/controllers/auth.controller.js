40772f12e5dd339920fe4a7c5710b638f93eed5f
Jatin Chaudhary <09jatin2005@gmail.com>
2024-07-31 20:48:51 +0530

    admin added

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -46,7 +46,7 @@ export const signin = async (req, res, next) => {
     if (!validPassword) {
       return next(errorHandler(400, "Invalid password"));
     }
-    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const token = jwt.sign({ id: validUser._id , isAdmin : validUser.isAdmin}, process.env.JWT_SECRET);
 
     const { password: pass, ...rest } = validUser._doc;
 
@@ -66,7 +66,7 @@ export const google = async (req, res, next) => {
   try {
     const user = await User.findOne({ email });
     if (user) {
-      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+      const token = jwt.sign({ id: user._id , isAdmin : user.isAdmin}, process.env.JWT_SECRET);
       const { password, ...rest } = user._doc;
       res
         .status(200)
@@ -90,7 +90,7 @@ export const google = async (req, res, next) => {
         profilePicture: photoURL,
       });
       const token = jwt.sign(
-        { id: NewUser._id, },
+        { id: NewUser._id, isAdmin : NewUser.isAdmin },
         process.env.JWT_SECRET
       );
       const { password, ...rest } = NewUser._doc;
