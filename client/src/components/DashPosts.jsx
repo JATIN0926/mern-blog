b87cc09d1b58d8b88a03dea0b3e27d347a74d593
Jatin Chaudhary <09jatin2005@gmail.com>
2024-08-05 08:56:19 +0530

    add show more functionality

--- client/src/components/DashPosts.jsx
+++ client/src/components/DashPosts.jsx
@@ -30,7 +30,23 @@ const DashPosts = () => {
     }
   }, [currentUser._id]);
 
-  const handleShowMore= ()=>{}
+  const handleShowMore = async () => {
+    const startIndex = userPosts.length;
+    try {
+      const res = await fetch(
+        `/api/post/getposts?userId=${currentUser._id}&startIndex=${startIndex}`
+      );
+      const data = await res.json();
+      if (res.ok) {
+        setUserPosts((prev) => [...prev, ...data.posts]);
+        if (data.posts.length < 9) {
+          setShowMore(false);
+        }
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
 
   const handleDeletePost = ()=>{}
 
