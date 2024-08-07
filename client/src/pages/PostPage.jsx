c134f96cb321d74ef88984db43fd8e6b384d6bf8
Jatin Chaudhary <09jatin2005@gmail.com>
2024-08-07 16:36:28 +0530

    scrollToTop + CTA

--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import { Button, Spinner } from "flowbite-react";
+import CallToAction from "../components/CallToAction";
+import CommentSection from "../components/CommentSection";
 
 const PostPage = () => {
   const { postSlug } = useParams();
@@ -67,10 +69,10 @@ const PostPage = () => {
         className="p-3 max-w-2xl mx-auto w-full post-content"
         dangerouslySetInnerHTML={{ __html: post && post.content }}
       ></div>
-      {/* <div className='max-w-4xl mx-auto w-full'>
+      <div className='max-w-4xl mx-auto w-full'>
         <CallToAction />
-      </div> */}
-      {/* <CommentSection postId={post._id} /> */}
+      </div>
+      <CommentSection postId={post._id} />
 
       <div className="flex flex-col justify-center items-center mb-5">
         <h1 className="text-xl mt-5">Recent articles</h1>
