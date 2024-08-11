ccfa6db790f3a06dc14e2f7664a0e54f6ee01d0e
Jatin Chaudhary <09jatin2005@gmail.com>
2024-08-11 17:34:24 +0530

    deploying configurations

--- api/index.js
+++ api/index.js
@@ -6,6 +6,8 @@ import authRoutes from "./routes/auth.route.js";
 import PostRoutes from "./routes/post.route.js";
 import commentRoutes from "./routes/comment.route.js";
 import cookieParser from "cookie-parser";
+import path from "path";
+
 dotenv.config();
 mongoose
   .connect(process.env.MONGO_URI)
@@ -15,17 +17,27 @@ mongoose
   .catch((error) => {
     console.log(error);
   });
+
+const __dirname = path.resolve();
+
 const app = express();
 
 // app.set('case sensitive routing', true);
 app.use(express.json());
-app.use(cookieParser())
+app.use(cookieParser());
 //routes
 
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/post", PostRoutes);
 app.use("/api/comment", commentRoutes);
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+});
+
 
 //middlewares
 
